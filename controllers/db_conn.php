69bcd69a17659bec63c449d0721e624c439d56cc
CoderCowMoo <broskichad38@gmail.com>
2023-01-22 11:57:22 +1100

    Moved database conn constants into a .env.php file.

--- controllers/db_conn.php
+++ controllers/db_conn.php
@@ -1,2 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "test");
+// the .env.php file at project root contains constants for the database connection
+// separate file to keep it out of the repo
+require __DIR__ . '/../.env.php';
+$conn = new mysqli($hostname, $user, $password, $database);
